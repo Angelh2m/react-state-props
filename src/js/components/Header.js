f80d7ad608485f5f8e7e81c2ad70b68912b87309
AngelH2m <angelh2m@icloud.com>
2017-08-20 22:15:06 -0400

    React App

---
+++ src/js/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Title from "./Header/Title";
+
+export default class Header extends React.Component {
+
+  // constructor(){
+  //   super();
+  //   this.state = {name: "will"};
+  // }
+  handleChange(e){
+    const title = e.target.value;
+
+    this.props.changeTitle(title);
+  }
+
+
+  render() {
+
+    return (
+      <div>
+        <Title title={this.props.title}/>
+      <input value={this.props.title} onChange={this.handleChange.bind(this)}/>
+      </div>
+    );
+  }
+}
